0aba1a837a76cc4e03b338f25623f99e483e3b2d
amani <akai@safaricom.co.ke>
2023-02-26 15:57:54 +0300

    added footer

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/App.css';
 import Header from './Header';
 import AboutUs from './AboutUs';
+import Footer from './Footer';
 
 function App() {
  return (
@@ -9,6 +10,7 @@ function App() {
      <div className="container-fluid">
        <Header />
        <AboutUs />
+       <Footer />
 
      </div>
    </React.Fragment>   
