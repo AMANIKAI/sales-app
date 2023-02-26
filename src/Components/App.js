deca7a543535b286e4c9bb6c95428d6ac2c17601
amani <akai@safaricom.co.ke>
2023-02-26 15:20:55 +0300

    added components

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+import Header from './Header';
+
+function App() {
+ return (
+   <React.Fragment>
+     <div className="container-fluid">
+       <Header />
+     </div>
+   </React.Fragment>   
+ )
+}
+
+export default App;
